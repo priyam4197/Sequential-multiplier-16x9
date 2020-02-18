64b64e4fe513c74ef44f00c76236545d68005af7
Priyam Upadhyay <iampriyam1@gmail.com>
2020-02-19 00:25:24 +0530

    Update TestBench_Seq.v

--- testbench/TestBench_Seq.v
+++ testbench/TestBench_Seq.v
@@ -9,18 +9,18 @@ reg RESET;
 wire [24:0]Prod;
 
 seq16x9 MULT(RESET,in_Mx,in_My,Prod,CLK);
-
-initial begin
-$monitor($time, " in_Mx = %b, in_My = %b, Prod = %h, CLK = %b", in_Mx,in_My,Prod,CLK );
-
-in_Mx = 16'hffff; in_My = 9'h1ff; RESET = 1'b1;
-//#100 in_Mx = 16'b1111101110101111; in_My = 9'b111101001;
-end
-
-initial begin
-CLK = 0;
-forever
-#5 CLK = !CLK;
-end
+  
+  initial
+    begin
+      $monitor($time, " in_Mx = %b, in_My = %b, Prod = %h, CLK = %b", in_Mx,in_My,Prod,CLK );
+      in_Mx = 16'hffff; in_My = 9'h1ff; RESET = 1'b1;
+      //#150 in_Mx = 16'b1111101110101111; in_My = 9'b111101001;
+    end
+  initial 
+    begin
+      CLK = 0;
+      forever
+        #5 CLK = !CLK;
+    end
 
 endmodule
