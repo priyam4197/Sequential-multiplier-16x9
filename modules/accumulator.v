6e2759e83fe43a0662827c62057564f8f4f17102
Priyam Upadhyay <iampriyam1@gmail.com>
2020-02-19 00:07:24 +0530

    Update accumulator.v

--- modules/accumulator.v
+++ modules/accumulator.v
@@ -5,17 +5,23 @@ module Accumulator(IN,LD_ACC,CLR_ACC,ACC,D_OUT,CLK);
 
 input [16:0]IN; 
 input LD_ACC, CLR_ACC, CLK;
+  
 output D_OUT; 
 output reg [16:0]ACC; //output reg[7:0]shiftReg;
+  
+  always @(posedge CLK) 
+    begin
+      if (CLR_ACC) 
+        begin
+          ACC <= 17'd0; 
+          //shiftReg <= 8'b0;
+        end
+      else 
+        if (LD_ACC) 
+          begin
+            ACC = IN;
+          end
+    end
+  assign D_OUT = ACC[0];
 
-always @(posedge CLK) begin
-if (CLR_ACC) begin
-ACC <= 17'd0; 
-//shiftReg <= 8'b0;
-end
-else if (LD_ACC) begin 
-ACC = IN;
-end
-end
-assign D_OUT = ACC[0];
-endmodule 
+endmodule
