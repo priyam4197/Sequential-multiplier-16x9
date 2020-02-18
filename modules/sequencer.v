85141b36c9f02b399cf4c389efc19d2d7a65cccc
Priyam Upadhyay <iampriyam1@gmail.com>
2020-02-18 23:36:31 +0530

    Add files via upload

---
+++ modules/sequencer.v
@@ -0,0 +1,19 @@
+//sequencer points to next state based on present step
+
+module sequencer(INAddr,CLK,RST,OUTAddr);
+
+input [3:0]INAddr;	//next state from control logic
+input CLK,RST;
+output reg [3:0]OUTAddr;	//output is present state signal to be fed to control ROM 
+
+always @(negedge CLK) begin
+if (~RST) begin		//RESET is active LOW
+OUTAddr = 4'b0000;
+end
+else
+begin
+OUTAddr = INAddr;
+end
+end
+
+endmodule
