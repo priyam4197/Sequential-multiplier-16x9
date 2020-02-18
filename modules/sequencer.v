20c5b6d758c641cf0b647e08e818ac8eee6790e6
Priyam Upadhyay <iampriyam1@gmail.com>
2020-02-18 23:38:12 +0530

    Update sequencer.v

--- modules/sequencer.v
+++ modules/sequencer.v
@@ -6,14 +6,16 @@ input [3:0]INAddr;	//next state from control logic
 input CLK,RST;
 output reg [3:0]OUTAddr;	//output is present state signal to be fed to control ROM 
 
-always @(negedge CLK) begin
-if (~RST) begin		//RESET is active LOW
-OUTAddr = 4'b0000;
-end
-else
-begin
-OUTAddr = INAddr;
-end
-end
+always @(negedge CLK) 
+  begin
+    if (~RST) 
+      begin		//RESET is active LOW
+        OUTAddr = 4'b0000;
+      end
+    else
+      begin
+        OUTAddr = INAddr;
+      end
+  end
 
 endmodule
